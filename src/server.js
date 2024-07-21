9c984e9f483580ea4204b3f460a5f5a79b5f1a06
Luca Ventura <117950527+luk444@users.noreply.github.com>
2024-07-21 04:09:15 -0300

    Update server.js

--- src/server.js
+++ src/server.js
@@ -1,11 +1,13 @@
-require("dotenv").config(); // Agrega esto al comienzo de tu archivo
+
+
+require("dotenv").config(); 
 
 const express = require("express");
 const router = express.Router();
 const cors = require("cors");
 const nodemailer = require("nodemailer");
 
-// server used to send send emails
+
 const app = express();
 app.use(cors());
 app.use(express.json());
